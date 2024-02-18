7d01aeb19ec3a03f663f0c0cf2d2baee72debe53
EkanshJuneja27 <ekansh.20004@gmail.com>
2024-02-18 16:53:36 +0530

    latest

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+
+const API_KEY = import.meta.env.VITE_OPENAI_API
+// "Explain things like you would to a person who doesn't know much"
+const systemMessage = { //  Explain things like you are an extremely experienced environment and life on land expert 
+  "role": "system", "content": "Explain things like you are an extremely experienced environment and life on land expert."
+}
+
+function App() {
+
+  const isBackgroundGreen = true;
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm Your Life on Land Expert! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChatGPT"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
+    };
+
+    const newMessages = [...messages, newMessage];
+    
+    setMessages(newMessages);
+
+    // Initial system message to determine functionality
+    // How it responds, how it talks, etc.
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
+    // Format messages for API
+    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
+    // So we need to reformat
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+
+    // Get the request body set up with the model we plan to use
+    // and the messages which we formatted above. We add a system message in the front to'
+    // determine how we want chatbot to act. 
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  // The system message DEFINES the logic of our chatbot
+        ...apiMessages // The messages from our chat with Chatbot
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
+  }
+
+  return (
+    <div className="chatbotapp">
+      <div style={{ backgroundColor: isBackgroundGreen ? 'lightgreen' : 'blue',}}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+      <h2 style={{ padding: "10px 20px", textAlign: "center", color: "red" }}>
+        Life on Land Expert
+      </h2>
+      <div style={{ position:"relative", textAlign: "left", height: "650px", width: "600px", }}>
+        <MainContainer>
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="Expert is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                console.log(message)
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
+      </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
